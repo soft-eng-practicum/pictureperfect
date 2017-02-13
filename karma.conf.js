51fb05d7e8ff5dadc5b9e86288a95852c85646f0
nklemenc <nklemenc@ggc.edu>
2017-02-13 01:28:15 -0500

    update

--- karma.conf.js
+++ karma.conf.js
@@ -2,87 +2,74 @@
 // Generated on Mon Feb 13 2017 00:04:36 GMT-0500 (Eastern Standard Time)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+      // base path that will be used to resolve all patterns (eg. files, exclude)
+      basePath: '',
 
+      // plugins starting with karma- are autoloaded
+      plugins: ['karma-chrome-launcher', 'karma-jasmine'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+      // frameworks to use
+      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: ['jasmine'],
 
+      // list of files / patterns to load in the browser
+      files: [
+        '*.js'
+      ],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'www/*.js',
-      'test/**/*Spec.js',
-    ],
+      // list of files to exclude
+      exclude: [
+      ],
 
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+      },
 
-    // list of files to exclude
-    exclude: [
-    ],
+      // test results reporter to use
+      // possible values: 'dots', 'progress'
+      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+      reporters: ['progress'],
 
+      // web server port
+      port: 9876,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+      // enable / disable colors in the output (reporters and logs)
+      colors: true,
 
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_INFO,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: true,
 
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ['Chrome', 'ChromeCanary', 'Firefox', 'Opera', 'IE'],
 
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-
-    var configuration = {
-    // other things
-
-    customLaunchers: {
+      // e.g see https://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
+      customLaunchers: {
         Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
+          base: 'Chrome',
+          flags: ['--no-sandbox']
         }
-    },
-};
+      },
 
-if (process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
-}
+      // Continuous Integration mode
+      // if true, Karma captures browsers, runs the tests and exits
+      singleRun: false,
 
-config.set(configuration);
-  })
+      // Concurrency level
+      // how many browser should be started simultaneous
+      concurrency: Infinity
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 }
